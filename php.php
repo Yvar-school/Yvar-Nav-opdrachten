bcb5fceee097ea6d0416999d94041ea520d154ac
Yvar-School <yvar.nanlohij001@fclive.nl>
2020-03-31 14:38:49 +0200

    Database

--- php.php
+++ php.php
@@ -1,16 +1,28 @@
-<DOCTYPE html>
-<html>
-    <?php 
+<?php 
+        try{
         $connection = new PDO('mysql:host=Localhost;dbname=article', 'root', '');
-    
-        $artikle = $connection->query('select 
+        }
+        catch( PDOException $e )
+        {
+            die( $e->getMessage() );
+        }
+        $test = $connection->query('select 
             artikel.id,
+            artikel.catoID,
+            categorien.categorie,
             artikel.Header,
             artikel.Content,
             artikel.Auteur
             from artikel
+            left join categorien 
+            on categorien.id = artikel.catoID
         ');
+        $artikel = $test->fetchAll();
+
+
     ?>
+<DOCTYPE html>   
+<html>
     <head>
         <style>
         	html{
@@ -26,6 +38,12 @@
         </style>
     </head>
     <body>
-        
+            <?php foreach($artikel as $row): ?>
+                <h2><?php echo $row['categorie'] ?></h2>
+                <h1><?php echo $row['Header'] ?></h1>
+                <p><?php echo $row['Content'] ?> </p>
+                <em>Auteur: <?php echo $row['Auteur'] ?></em>
+            <?php endforeach;?>
+        <!--<pre> <?php// echo print_r($artikel); ?></pre>-->
     </body>
 </html>
